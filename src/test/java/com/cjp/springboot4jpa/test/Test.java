be4e80f6e7521f5c611551123eaa43afec815165
chijunping <378344307@qq.com>
2018-06-17 13:19:25 +0800

    test

---
+++ src/test/java/com/cjp/springboot4jpa/test/Test.java
@@ -0,0 +1,24 @@
+package com.cjp.springboot4jpa.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * created by cjp on 2018/6/17
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        List<StringBuffer> listTest = new ArrayList<>();
+        //快捷键 list.for
+        for (StringBuffer buffer : listTest) {
+
+        }
+        //快捷键 fori
+        for (int i = 0; i < 123; i++) {
+
+        }
+
+
+    }
+}
